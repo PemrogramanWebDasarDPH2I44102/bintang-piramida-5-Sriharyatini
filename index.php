0efdbb59a07b9e45bc3cb88577ff941bedf9f7f0
Sriharyatini <42663121+Sriharyatini@users.noreply.github.com>
2018-08-24 10:42:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
-//tuliskan loopingnya di sini
+//tuliskan loopingnya disini
+echo "Piramida <br>";
+for ($a = 1; $a>=5; $a++)
+{
+	for ($b =1; $b<=$a; $b++){
+		echo "*";
+	}
+	echo "<br>";
+}
+
 ?>
